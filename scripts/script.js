0419987d15ab5f65bc6a7a987cf78f5d1303fcbc
jayspm <62362003+jayspm@users.noreply.github.com>
2022-10-06 22:12:22 +1100

    set up html pages
    set up html pages: (1) index.html (2) createquiz.html (3) dashboard.html (4) gameplay.html (5) userprofile.html (6) admin.html
    
    worked on index.html => login and register pages

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+/**
+ * Author: G13 - 2022-HS2-COS60010-Technology Enquiry Project
+ * Target: index.html
+ * Purpose: Design for onclick 
+ * Created: 6/10/2022
+ * Last updated: 00/00/2022
+ */
+
+"use strict";
+window.addEventListener("load", init);
+
+function init() {
+    var loginBtn = document.getElementById("loginBtn");
+    var registerBtn = document.getElementById("registerBtn");
+
+    loginBtn.addEventListener("click", login);
+    registerBtn.addEventListener("click", register);
+}
+
+function register() {
+    var x = document.getElementById("login");
+    var y = document.getElementById("register");
+    var z = document.getElementById("btn");
+
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login() {
+    var x = document.getElementById("login");
+    var y = document.getElementById("register");
+    var z = document.getElementById("btn");
+
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0";
+}
